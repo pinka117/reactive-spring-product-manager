ffd5f89e6da04622f912b9837c2d7db8934926f3
linda <linda.paolieri.27@gmail.com>
2018-03-13 13:34:28 +0100

    inizialization

---
+++ src/main/java/com/reactive/spring/productManager/ProductManagerApplication.java
@@ -0,0 +1,12 @@
+package com.reactive.spring.productManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerApplication.class, args);
+	}
+}
