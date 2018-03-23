a136dfd6268312662ad5870a2956d82d19f62bf1
Linda Paolieri <linda.paolieri.27@gmail.com>
2018-03-23 15:05:44 +0100

    change pom profile for jacoco

--- src/test/java/com/reactive/spring/product/manager/controller/ItemWebControllerTest.java
+++ src/test/java/com/reactive/spring/product/manager/controller/ItemWebControllerTest.java
@@ -1,11 +1,13 @@
 package com.reactive.spring.product.manager.controller;
 
 
+import com.reactive.spring.product.manager.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ItemWebControllerTest {
     @Autowired
     private MockMvc mvc;
+    @MockBean
+    UserRepository userRepository;
 
     @Test
     @WithAnonymousUser
