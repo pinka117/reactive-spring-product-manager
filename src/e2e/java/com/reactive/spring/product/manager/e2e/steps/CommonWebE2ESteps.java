0e9f454796f0c55ca5621d60b6fe1aef904d0d40
linda <linda.paolieri.27@gmail.com>
2018-04-03 13:47:22 +0200

    refactor code e2e tests
    Signed-off-by: linda <linda.paolieri.27@gmail.com>

---
+++ src/e2e/java/com/reactive/spring/product/manager/e2e/steps/CommonWebE2ESteps.java
@@ -0,0 +1,71 @@
+package com.reactive.spring.product.manager.e2e.steps;
+
+import com.reactive.spring.product.manager.controller.webdriver.pages.*;
+import cucumber.api.java8.En;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+public class CommonWebE2ESteps implements En {
+    protected static int port =
+            Integer.parseInt(System.getProperty("server.port", "8080"));
+    protected static String url = "http://localhost:" + port;
+    protected WebDriver webDriver;
+    protected HomePage homePage;
+    protected LoginPage loginPage;
+    protected NewPage newPage;
+    protected AbstractPage redirectedPage;
+    protected AbstractPage currentPage;
+    protected AdminHomePage adminHomePage;
+
+    public CommonWebE2ESteps() {
+        Given("^The user is on Home Page$", () -> {
+            homePage = HomePage.to(webDriver);
+            this.currentPage = homePage;
+        });
+        Given("^The user logged in as an admin$", () -> {
+            gotoLoginPage();
+            validLogin();
+            pressLogin();
+        });
+        Then("^The user is redirected to Home Page$", () -> {
+
+            assertThat(redirectedPage).isInstanceOf(HomePage.class);
+        });
+        Then("^A message \"([^\"]*)\" must be shown$", (String msg) -> {
+            assertThat(currentPage.getBody()).contains(msg);
+        });
+        Given("^The user isn't logged in$", () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (!(authentication == null)) {
+                fail("User isn't logged out");
+            }
+        });
+        When("^The user navigates on \"new\" page$", () -> {
+            newPage = NewPage.to(webDriver);
+            this.currentPage = newPage;
+        });
+    }
+
+    protected void gotoLoginPage() {
+        loginPage = LoginPage.to(webDriver);
+        currentPage = loginPage;
+    }
+
+    protected void pressLogin() {
+        redirectedPage = loginPage.pressLogin(HomePage.class);
+    }
+
+    protected void validLogin() {
+        loginPage.submitForm("admin", "admin");
+    }
+
+    protected void setPort() {
+        AbstractPage.port = port;
+        webDriver = new ChromeDriver();
+    }
+}
--- src/e2e/java/com/reactive/spring/product/manager/e2e/steps/view/ViewWebE2ESteps.java
+++ src/e2e/java/com/reactive/spring/product/manager/e2e/steps/view/ViewWebE2ESteps.java
@@ -1,45 +1,15 @@
 package com.reactive.spring.product.manager.e2e.steps.view;
 
-import com.reactive.spring.product.manager.controller.webdriver.pages.AbstractPage;
-import com.reactive.spring.product.manager.controller.webdriver.pages.AdminHomePage;
-import com.reactive.spring.product.manager.controller.webdriver.pages.HomePage;
-import com.reactive.spring.product.manager.controller.webdriver.pages.LoginPage;
+import com.reactive.spring.product.manager.e2e.steps.CommonWebE2ESteps;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import cucumber.api.java8.En;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import static org.assertj.core.api.Assertions.fail;
 import static org.junit.Assert.assertTrue;
 
-public class ViewWebE2ESteps implements En {
-    private static int port =
-            Integer.parseInt(System.getProperty("server.port", "8080"));
-    private static String url = "http://localhost:" + port;
-    private WebDriver webDriver;
-    private HomePage homePage;
-    private LoginPage loginPage;
-    private AdminHomePage adminHomePage;
-
+public class ViewWebE2ESteps extends CommonWebE2ESteps {
     public ViewWebE2ESteps() {
-        Given("^The user is on Home Page$", () -> {
-
-            homePage = HomePage.to(webDriver);
-
-
-        });
         And("^The db is not empty$", () -> {
-
-
-        });
-        Given("^The user logged in as an admin$", () -> {
-            gotoLoginPage();
-            validLogin();
-            pressLogin();
-
         });
         Then("^Items are displayed$", () -> {
             assertTrue(homePage.getBody().contains("name1"));
@@ -47,83 +17,40 @@ public class ViewWebE2ESteps implements En {
             assertTrue(homePage.getBody().contains("location1"));
         });
         And("^There is a button \"Modify\"$", () -> {
-
-
             assertTrue(homePage.modifyIsEnabled());
-
-
         });
         And("^There is a button \"Remove\"$", () -> {
-
-
             assertTrue(homePage.removeIsEnabled());
-
-
         });
         And("^There is a button \"Add\"$", () -> {
-
-
             assertTrue(homePage.addIsEnabled());
-
-
-        });
-        Given("^The user isn't logged in$", () -> {
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            if (!(authentication == null)) {
-                fail("User isn't logged out");
-            }
-            homePage = HomePage.to(webDriver);
-
         });
         And("^There isn't a button \"Modify\"$", () -> {
-
             try {
                 homePage.modifyIsEnabled();
                 fail("Button enabled");
             } catch (Exception e) {
-
             }
-
         });
         And("^There isn't a button \"Remove\"$", () -> {
             try {
                 homePage.removeIsEnabled();
                 fail("Button enabled");
             } catch (Exception e) {
-
             }
-
         });
         And("^There isn't a button \"Add\"$", () -> {
             try {
                 homePage.addIsEnabled();
                 fail("Button enabled");
             } catch (Exception e) {
-
             }
-
         });
-    }
-
-    private void gotoLoginPage() {
-        loginPage = LoginPage.to(webDriver);
-
-    }
-
-    private void pressLogin() {
-        loginPage.pressLogin(HomePage.class);
-
-
-    }
-
-    private void validLogin() {
-        loginPage.submitForm("admin", "admin");
     }
 
     @Before
     public void setup() {
-        AbstractPage.port = port;
-        webDriver = new ChromeDriver();
+        setPort();
     }
 
     @After
